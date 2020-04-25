cda0af2dda335b4780b0283a09f60f0981f05534
Amund Eggen Svandal <aesvandal@gmail.com>
2020-04-25 16:26:52 +0200

    Remove unnecessary code

--- src/ball/main.cpp
+++ src/ball/main.cpp
@@ -11,9 +11,6 @@ int main() {
 	root->state = new Gamestate{ true, 0, 0, 0, 0 };
 	std::cout << "Current state:" << std::endl << sToString(root->state) << "\n" << std::endl;
 
-	root->amtChildren = 2;
-	root->children = new Node*[2];
-
 	unsigned int depth = 15;
 
 	Evaluation e = bestAction(root, depth);
