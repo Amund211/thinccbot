0435d7de5ac99e849811699eb3af9855346ab423
Amund Eggen Svandal <aesvandal@gmail.com>
2020-07-19 10:57:12 +0200

    Small change to `bestAction`
    Rename the action variable and raise an exception if it was called on a
    terminal state

--- src/trees.cpp
+++ src/trees.cpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <cmath>
 #include <algorithm>
+#include <stdexcept>
 
 #include "game.h"
 
@@ -11,7 +12,7 @@ Evaluation bestAction(Gamestate const* statep, unsigned int depth)
 	// State must not be terminal
 	float value;
 	float childValue;
-	Action* bestAction = nullptr;
+	Action* action = nullptr;
 
 	std::vector<Gamestate*> states;
 	std::vector<Action*> actions;
@@ -25,16 +26,20 @@ Evaluation bestAction(Gamestate const* statep, unsigned int depth)
 		if (childValue > value) {
 			// New best action
 			value = childValue;
-			if (bestAction) {
-				deleteAction(bestAction);
+			if (action) {
+				deleteAction(action);
 			}
-			bestAction = actions[i];
+			action = actions[i];
 		} else {
 			deleteAction(actions[i]);
 		}
 		deleteState(states[i]);
 	}
-	return { bestAction, value };
+
+	if (!action)
+		throw std::runtime_error("Terminal state given to bestAction");
+
+	return { action, value };
 }
 
 float negamax(Gamestate const* statep, unsigned int depth, float alpha, float beta)
